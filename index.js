026bbff3ebffa1acabd128f7ab3e0c086b1e930e
Md.Ashraful Haque <ashrafulhaque@users.noreply.github.com>
2024-10-22 11:54:30 +0600

    Successfully used dotenv in the sever

--- index.js
+++ index.js
@@ -1,24 +1,45 @@
 const express = require("express");
 const cors = require("cors");
+require("dotenv").config();
+const { MongoClient, ServerApiVersion } = require("mongodb");
 
 const app = express();
-
 const port = process.env.PORT || 5000;
 
-// Enable CORS for all origins
 app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.q1o0o.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server (don't close the connection here)
+    await client.connect();
+
+    // Ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Successfully connected to MongoDB!");
+  } catch (error) {
+    console.error("Error connecting to MongoDB:", error);
+  }
+}
+
+run().catch(console.dir);
 
 app.get("/", (req, res) => {
-  res.send("MERNSTACK Server for Carshop is Started");
+  res.send("Welcome to the express MongoDB CRUD server!");
 });
 
-// Handle undefined routes (404 Not Found)
-app.use((req, res) => {
-  res.status(404).send({ error: "Endpoint Not Found" });
-});
-
-app.listen(port, (req, res) => {
-  console.log(`MERNSTACK Server for Carshop is Running on port ${port}`);
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
 });
 
 module.exports = app;
